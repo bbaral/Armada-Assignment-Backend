469cf1d557a9e6c19e8475a20a9ef5127bc6eac3
bbaral <baralbikram@hotmail.com>
2019-11-19 00:25:29 -0500

    DAO, controller and impl implemented

--- src/main/java/com/armada/backend/Implementation/GroceryListImpl.java
+++ src/main/java/com/armada/backend/Implementation/GroceryListImpl.java
@@ -1,6 +1,21 @@
 package com.armada.backend.Implementation;
 
 import com.armada.backend.DAO.GroceryListDAO;
+import com.armada.backend.pojo.GroceryList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public class GroceryListImpl implements GroceryListDAO {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Override
+    public List<GroceryList> getAllList() {
+        return mongoTemplate.findAll(GroceryList.class);
+    }
 }
--- src/main/java/com/armada/backend/controller/GroceryListController.java
+++ src/main/java/com/armada/backend/controller/GroceryListController.java
@@ -1,8 +1,31 @@
 package com.armada.backend.controller;
 
 
+import com.armada.backend.Implementation.GroceryListImpl;
+import com.armada.backend.pojo.GroceryList;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
+@RequestMapping("/grocery")
 public class GroceryListController {
+
+    @Autowired
+    private GroceryListImpl groceryListImpl;
+
+    private Logger logger = LoggerFactory.getLogger(GroceryListController.class);
+
+    @RequestMapping(value = "/getAllList", method = RequestMethod.GET, consumes = "application/json", produces = "application/json")
+    public List<GroceryList> getAllList() {
+        return groceryListImpl.getAllList();
+    }
 }
