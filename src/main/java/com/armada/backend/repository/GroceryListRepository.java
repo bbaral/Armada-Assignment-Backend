28d895b1ebdc65f72dcd4bd0786bafa4d07869a8
bbaral <baralbikram@hotmail.com>
2019-11-19 01:11:45 -0500

    Repo error

--- src/main/java/com/armada/backend/repository/GroceryListRepository.java
+++ src/main/java/com/armada/backend/repository/GroceryListRepository.java
@@ -1,8 +1,9 @@
 package com.armada.backend.repository;
 
+import com.armada.backend.pojo.GroceryList;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GroceryListRepository extends MongoRepository {
+public interface GroceryListRepository extends MongoRepository<GroceryList, String> {
 }
